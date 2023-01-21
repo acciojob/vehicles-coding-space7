2e8eff19c8447e72f1cf5be200173b2b02c3bbd2
coding-space7 <sawbharat2001@gmail.com>
2023-01-21 22:04:08 +0530

    Fourth commit

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,14 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+
         //Use arbitrary values for parameters which are not mentioned
+        super(name,4,2,6,isManual,"SportsCar",2);
+
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -17,12 +20,40 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
+        int newSpeed = 0;
+        newSpeed = getCurrentSpeed()+rate; // cars current speed
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            super.stop();
+            setCurrentGear(1);
         }
-        //for all other cases, change the gear accordingly
+        //for all other cases, change the gear accordingly  changeSpeed(newSpeed, getCurrentDirection());
 
+        else if(newSpeed > 0 && newSpeed <=50 ) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(1); // using setter method to fetch the parents class method
+        }
+        else if(newSpeed>= 51 && newSpeed<=100){
+            setCurrentGear(2);
+            setCurrentSpeed(newSpeed);
+        }
+        else if(newSpeed >=101 && newSpeed<=150) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(3);
+        }
+        else if(newSpeed >=151 && newSpeed<=200) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(4);
+        }
+        else if(newSpeed >=201 && newSpeed<=250) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(5);
+        }
+        else if(newSpeed >250) {
+            setCurrentSpeed(newSpeed);
+            setCurrentGear(6);
+        }
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
         }
